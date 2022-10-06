ab1d246a091bb346578e63aae39e71cda7e77c1d
colio1 <randy.colio@colorado.edu>
2022-10-06 06:44:02 -0700

    added writer

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -43,8 +43,9 @@ else
 fi
 
 #echo "Removing the old writer utility and compiling as a native application"
-make clean
-make CROSS_COMPILE=aarch64-none-linux-gnu- all
+
+#imake clean
+#make CROSS_COMPILE=aarch64-none-linux-gnu- all
 
 for i in $( seq 1 $NUMFILES)
 do
